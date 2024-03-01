a6e9e938a4b5f3f335a216fe5762db269e378fcd
Nikita Babitsyn <nitubro1617@gmail.com>
2024-03-01 19:23:38 +0500

    created custom hook for getting data from api

---
+++ src/hooks/useGetData.js
@@ -0,0 +1,31 @@
+import md5 from "js-md5";
+import {useState} from "react";
+
+const getPassword=()=>{
+    const today = new Date();
+    const year = today.getUTCFullYear().toString();
+    let month = (today.getUTCMonth() + 1).toString();
+    if (month<10){
+        month ="0"+month
+    }
+    let day = today.getUTCDate().toString();
+    if (day<10){
+        day="0"+day
+    }
+    return md5(`Valantis_${year + month + day}`)
+}
+
+async function UseGetData({url,body}) {
+    const password = getPassword()
+        const response= await fetch(`${url}`,{
+            method: "POST",
+            headers: {
+                "X-Auth": password,
+                "Content-Type": "application/json",
+            },
+            body:JSON.stringify(body)
+        }).then(res=>res)
+    return response.json()
+}
+
+export default UseGetData;
