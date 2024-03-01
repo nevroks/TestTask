ab7594fb2cd3bb30dc75b01fb42973b0535a4965
Nikita Babitsyn <nitubro1617@gmail.com>
2024-03-01 12:43:17 +0500

    added react router dom logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,20 @@
-import axios from "axios";
-import CryptoJS from"crypto-js"
-import md5 from "md5"
+import ProductItemList from "./components/productitemlist/ProductItemList.jsx";
+import {Route, Routes} from "react-router-dom";
+import Layout from "./components/layout/Layout.jsx";
+import MainPage from "./pages/mainpage/MainPage.jsx";
+
+
+
 function App() {
-    function getData() {
-        const password="Valantis"
-        const today = new Date();
-        const year = today.getUTCFullYear().toString();
-        const month = (today.getUTCMonth() + 1).toString(); // Месяцы в JavaScript начинаются с 0
-        const day = today.getUTCDate().toString();
-        let stamp=md5(`${password}_${year+month+day}`)
-        axios({
-          url: "https://api.valantis.store:41000",
-          method: "POST",
-          headers: {
-              'Content-Type': 'application/json',
-              "X-Auth": `${stamp}`,
-          },
-        body:JSON.stringify({
-            "action": "filter",
-            "params": {"price": 17500.0}
-        })
-      }).then(response => console.log(response))
-  }
+
   return (
     <>
-      <button onClick={getData}>Отправить запрос</button>
+        <Routes>
+             <Route path="/" element={<Layout/>}>
+                <Route index element={<MainPage/>}/>
+            </Route>
+        </Routes>
+      <ProductItemList/>
     </>
   )
 }
