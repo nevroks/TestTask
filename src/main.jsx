d4a2f3e0a08c3cb92732129d8784e7325399186e
Nikita Babitsyn <nitubro1617@gmail.com>
2024-03-04 20:43:14 +0500

    wrapped project witch provider of react context

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from "react-router-dom";
+import ProductArrayProvider from "./providers/ProductArrayProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-        <App />
-    </BrowserRouter>
+    <ProductArrayProvider>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
+            <App />
+        </BrowserRouter>
+    </ProductArrayProvider>
 )
