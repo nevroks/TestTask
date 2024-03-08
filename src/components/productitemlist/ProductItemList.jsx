1bd1fad4cc6d9a8eadd753f583c52184cd281ff7
Nikita Babitsyn <nitubro1617@gmail.com>
2024-03-08 13:42:39 +0500

    working variant of product item list witch filtering

--- src/components/productitemlist/ProductItemList.jsx
+++ src/components/productitemlist/ProductItemList.jsx
@@ -1,22 +1,27 @@
-import React, {lazy, Suspense, useContext, useEffect, useState} from 'react';
+import React, {useContext, useEffect, useMemo, useState} from 'react';
 import useGetData from "../../hooks/useGetData.js";
 import classes from "./productsItemlist.module.css";
 import {ProductArrayContext} from "../../providers/ProductArrayProvider.jsx";
 import ProductItem from "../productitem/ProductItem.jsx";
+import {ShopPageContext} from "../../providers/ShopPageProvider.jsx";
 
 
-const ProductItemList = ({body,currentPage,productsPerPage,filtered,searchQuery}) => {
+const ProductItemList = ({body,productsPerPage,filtered,searchQuery}) => {
     const {productsArray,setProductsArray}=useContext(ProductArrayContext)
+    const {currentPage}=useContext(ShopPageContext)
+
+    const [visitedPages,setVisitedPages]=useState([])
 
     const lastProductOnPage=currentPage*productsPerPage
     const firstProductOnPage=lastProductOnPage-productsPerPage
     const currentProductPage=productsArray.slice(firstProductOnPage,lastProductOnPage)
-    console.log(searchQuery)
-    let filteredBody = {
-        "action": "filter",
-        "params": {"product": `${searchQuery}`}}
 
-    const [visitedPages,setVisitedPages]=useState([])
+    const filteredBody =useMemo(()=>{
+        return {
+            "action": "filter",
+            "params": {"product": `${searchQuery.toLowerCase()}`}}
+    },[searchQuery])
+
     useEffect(()=>{
         setVisitedPages([...visitedPages,currentPage])
     },[currentPage])
@@ -25,23 +30,17 @@ const ProductItemList = ({body,currentPage,productsPerPage,filtered,searchQuery}
             useGetData({url:'https://api.valantis.store:41000/',body:filteredBody}).then(data=>setProductsArray(data.result))
             :
             visitedPages.includes(currentPage) ? 1 :
-            useGetData({url:'https://api.valantis.store:41000/',body}).then(data=>setProductsArray([...productsArray,data.result].flat()))
-    },[filtered,body])
+                useGetData({url:'https://api.valantis.store:41000/',body}).then(data=>setProductsArray([...productsArray,data.result].flat()))
+    },[filtered,body,filteredBody])
 
-
-    console.log(productsArray)
+    // useEffect(()=>{
+    //     if (isFilteredByPriceOrBrand){
+    //         useGetData({url:'https://api.valantis.store:41000/',body:filteredBody}).then(data=>setFilteredArray(data.result))
+    //     }
+    // },[isFilteredByPriceOrBrand])
     return (
         <>
         <div className={classes.ProductsList}>
-            {/*{!searchedArray.length ?*/}
-            {/*    currentProductPage.map(product=> {*/}
-            {/*        return <ProductItem key={product} productId={product}/>*/}
-            {/*    })*/}
-            {/*    :*/}
-            {/*    searchedArray.map(product=> {*/}
-            {/*        return <ProductItem key={product} productId={product}/>*/}
-            {/*    })*/}
-            {/*}*/}
             {currentProductPage.map(product=>{return <ProductItem key={product} productId={product}/>})}
         </div>
         </>
