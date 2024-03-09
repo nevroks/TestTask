f2972c81de194b361b21450378abd149616333e3
Nikita Babitsyn <nitubro1617@gmail.com>
2024-03-09 15:17:33 +0500

    added product page to routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {Route, Routes} from "react-router-dom";
 import Layout from "./components/layout/Layout.jsx";
 import {lazy} from "react";
 import ShopPageProvider from "./providers/ShopPageProvider.jsx";
+import ProductPage from "./pages/productpage/ProductPage.jsx";
 const MainPage = lazy(()=>import("./pages/mainpage/MainPage.jsx"))
 
 
@@ -12,7 +13,9 @@ function App() {
     <>
         <Routes>
              <Route path="/" element={<Layout/>}>
-                     <Route index element={<ShopPageProvider><MainPage/></ShopPageProvider>}/>
+                     <Route index element={<MainPage/>}/>
+                    <Route path="product" element={<ProductPage/>}/>
+                    <Route path="product/:id" element={<ProductPage/>}/>
             </Route>
         </Routes>
     </>
---
+++ src/pages/productpage/ProductPage.jsx
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from 'react';
+import {useNavigate, useParams} from "react-router-dom";
+import useGetData from "../../hooks/useGetData.js";
+import Button from "../../components/Ui/button/Button.jsx";
+
+const ProductPage = () => {
+    const {id}=useParams()
+    const [product,setProduct]=useState({})
+    const navigate=useNavigate()
+    const body={
+        "action": "get_items",
+        "params": {"ids": [`${id}`]}
+    }
+    console.log(id)
+    useEffect(()=>{
+        useGetData({url:"https://api.valantis.store:41000/",body}).then(data=>{setProduct(data.result[0])})
+    },[id])
+    const handleClick=()=>{
+        navigate(-1)
+    }
+    return (
+        <div>
+            <Button onClick={handleClick}>Назад</Button>
+            <h2>{product.product}</h2>
+            <p>{product.price} Рублей</p>
+            <p>{product.brand === null ? "Бренд неизвестен" : product.brand}</p>
+        </div>
+    );
+};
+
+export default ProductPage;
